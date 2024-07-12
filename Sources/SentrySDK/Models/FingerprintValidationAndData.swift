9fb7b0b0eb975a336df2f008225e593afd7dfc4f
John Ayres <john_ayres@verizon.net>
2024-07-12 11:53:12 -0500

    Updated comments.

--- Sources/SentrySDK/Models/FingerprintValidationAndData.swift
+++ Sources/SentrySDK/Models/FingerprintValidationAndData.swift
@@ -8,9 +8,12 @@
 import Foundation
 
 /**
- THIS STRUCT IS IN FLUX AND IS SUBJECT TO CHANGE.
+ Contains a value indicating if the fingerprint on the sensor matches the one recorded during enrollment, and any data stored on the card during the enrollment process.
  */
 public struct FingerprintValidationAndData {
+    /// `True` if the scanned fingerprint matches the one recorded during enrollment; otherwise `false`.
     public let doesFingerprintMatch: Bool
+    
+    /// Contains any data stored during the enrollment process. If no data was stored during enrollment, this array is empty.
     public let storedData: [UInt8]
 }
