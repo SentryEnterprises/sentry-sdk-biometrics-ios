e6b53034575bbb9bafb09bbd110e242c6b0b5d61
John Ayres <john_ayres@verizon.net>
2024-08-28 11:41:04 -0500

    Changed internal dependency 'Security' to 'SentrySecurity' to avoid naming collisions encountered by 3rd parties.

--- Package.swift
+++ Package.swift
@@ -19,12 +19,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Security",
+            name: "SentrySecurity",
             publicHeadersPath: "include"),
         
         .target(
             name: "SentrySDK",
-            dependencies: ["Security"]),
+            dependencies: ["SentrySecurity"]),
         
         .testTarget(
             name: "SentrySDKTests",
